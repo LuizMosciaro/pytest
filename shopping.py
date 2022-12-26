19fc273b320bc59416af713d5f20f49524efd457
luizmosciaro <edu.mosciaro@live.com>
2022-12-25 21:09:52 -0400

    Git inicial

---
+++ shopping.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class ShoppingCart:
+    def __init__(self,max_cart_size:int) -> None:
+        self.items: List[str] = []
+        self.max_cart_size = max_cart_size
+
+    def add(self,item:str):
+        if self.size() == self.max_cart_size:
+            raise OverflowError('Cant add more items to cart')
+        self.items.append(item)
+
+    def size(self) -> int:
+        return len(self.items)
+
+    def get_items(self) -> List[str]:
+        return self.items
+
+    def get_total_price(self,price_map):
+        total_price = 0
+        for item in self.items:
+            total_price += price_map.get(item)
+        return total_price
+
+#if __name__=='__main__':
+#    price_map = {'Apple':1.50,'Banana':2.00}
---
+++ test_shopping.py
@@ -0,0 +1,34 @@
+from shopping import ShoppingCart
+import pytest
+
+#Fixtures = Faz nao ser preciso instanciar o objeto/classe/configuracoes individualmente
+@pytest.fixture
+def cart():
+    #Todas configuracoes vêm aqui etc...
+    return ShoppingCart(5)
+
+def test_can_add_item_to_cart(cart):
+    #cart = ShoppingCart(3)
+    cart.add('Apple')
+    assert cart.size() == 1
+
+def test_when_item_added_then_cart_contains_item(cart):
+    #cart = ShoppingCart(2)
+    cart.add('Apple')
+    assert 'Apple' in cart.get_items()
+
+def test_when_add_more_than_max_items_to_list(cart):
+    #cart = ShoppingCart(5)
+    for _ in range(5):
+        cart.add('Apple')
+    
+    with pytest.raises(OverflowError):
+        cart.add('Banana')
+
+def test_can_get_total_price(cart):
+    #cart = ShoppingCart(5)
+    cart.add('Apple')
+    cart.add('Banana')
+
+    price_map = {'Apple':1.50,'Banana':2.00}
+    assert cart.get_total_price(price_map) == 3.50
